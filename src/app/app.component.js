1ef018fb5911d8cdbb0be2aa2dfbc6bd14da50a8
ROOT\thangavele <thangavele@syncfusion.com>
2017-12-20 19:25:30 +0530

    incident(195013): Dynamic changes done

--- src/app/app.component.js
+++ src/app/app.component.js
@@ -9,15 +9,23 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var core_1 = require("@angular/core");
+var ej2_ng_dropdowns_1 = require("@syncfusion/ej2-ng-dropdowns");
 var ej2_data_1 = require("@syncfusion/ej2-data");
 var AppComponent = (function () {
     function AppComponent() {
         var _this = this;
+        this.ddlValue = 1000;
+        this.fullData = [];
         // maps the appropriate column to fields property
         this.fields = { text: 'Number', value: 'Id' };
         //set the placeholder to DropDownList input
         this.text = "Select a number";
-        this.dataQuery = new ej2_data_1.Query().take(50);
+        // Open event callback function
+        this.onOpen = function (e) {
+            // override the popup position to avoid the confusion with bottom textbox
+            _this.ddlObj.popupObj.offsetY = -(_this.ddlObj.inputWrapper.container.offsetHeight + 2); // 2 for border of textbox wrapper element
+        };
+        // filtering event callback function
         this.onFiltering = function (e) {
             var query = new ej2_data_1.Query();
             // frame the query based on search string with filter type.
@@ -25,18 +33,39 @@ var AppComponent = (function () {
             // pass the filter data source, filter query to updateData method.
             e.updateData(_this.data, query);
         };
-        this.data = [];
         for (var i = 0; i < 10000; i++) {
-            this.data[i] = { Number: (1001 + i).toString(), Id: i };
+            this.fullData[i] = { Number: (1001 + i).toString(), Id: i };
         }
+        // Get the first 50 data items from fullData and assign it to this.initialData.
+        this.initialData = new ej2_data_1.DataManager(this.fullData).executeLocal(new ej2_data_1.Query().take(50));
+        // Get the DropDownList initial render value from fullData and assign it to this.initialData.
+        var secondData = new ej2_data_1.DataManager(this.fullData).executeLocal(new ej2_data_1.Query().where('Id', 'equal', this.ddlValue));
+        // Concat the this.initialData and secondData and then assign it to this.data for DropDownList dataSource
+        this.data = this.initialData.concat(secondData);
+        // Set the current DropDownList dataSource to this.initialData.
+        this.initialData = this.data;
     }
+    AppComponent.prototype.onChange = function (e) {
+        // Get the current value item from full data.
+        var secondData = new ej2_data_1.DataManager(this.fullData).executeLocal(new ej2_data_1.Query().where('Id', 'equal', e.target.valueAsNumber));
+        // Concat the current value item with initial loaded data.
+        this.data = this.initialData.concat(secondData);
+        // Set the current DropDownList dataSource to this.initialData.
+        this.initialData = this.data;
+        // Set the current numeric textbox value to DropDownList value property.
+        this.ddlValue = e.target.valueAsNumber;
+    };
     return AppComponent;
 }());
+__decorate([
+    core_1.ViewChild('samples'),
+    __metadata("design:type", ej2_ng_dropdowns_1.DropDownListComponent)
+], AppComponent.prototype, "ddlObj", void 0);
 AppComponent = __decorate([
     core_1.Component({
         selector: 'my-app',
         // specifies the template string for the DropDownList component with change event
-        template: "<ej-dropdownlist id='ddlelement' #samples [allowFiltering]='true' (filtering)='onFiltering($event)' [query]='dataQuery' [dataSource]='data' [fields]='fields' [placeholder]='text'>\n  <template #footerTemplate=\"\" let-data=\"\">\n  <div style=\"text-align:center;line-height:40px;background-color:#DCDCDC;\">Search more result</div>\n  </template>\n  </ej-dropdownlist>"
+        template: "<ej-dropdownlist id='ddlelement' #samples  [value]='ddlValue' [allowFiltering]='true' (open)='onOpen($event)' (filtering)='onFiltering($event)'  [dataSource]='data' [fields]='fields' [placeholder]='text'>\n  <template #footerTemplate=\"\" let-data=\"\">\n  <div style=\"text-align:center;line-height:40px;background-color:#DCDCDC;\">Search more result</div>\n  </template>\n  </ej-dropdownlist>\n  <h4>Type value</h4>\n  <input type=\"number\" [value]='1000' (input)='onChange($event)' style=\"padding:5px;width:100%;margin-top: 20px;\"/>\n  "
     }),
     __metadata("design:paramtypes", [])
 ], AppComponent);
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,35 +1,67 @@
-import { Component } from '@angular/core';
-import { DropDownListComponent, FilteringEventArgs } from '@syncfusion/ej2-ng-dropdowns';
-import { Query } from '@syncfusion/ej2-data';
+import { Component, ViewChild } from '@angular/core';
+import { DropDownListComponent, FilteringEventArgs, PopupEventArgs } from '@syncfusion/ej2-ng-dropdowns';
+import { Query, Predicate, DataManager } from '@syncfusion/ej2-data';
 import { EmitType } from '@syncfusion/ej2-base';
+import { debug } from 'util';
+import { ChangeArgs } from '@syncfusion/ej2-buttons';
 
 @Component({
   selector: 'my-app',
   // specifies the template string for the DropDownList component with change event
-  template: `<ej-dropdownlist id='ddlelement' #samples [allowFiltering]='true' (filtering)='onFiltering($event)' [query]='dataQuery' [dataSource]='data' [fields]='fields' [placeholder]='text'>
+  template: `<ej-dropdownlist id='ddlelement' #samples  [value]='ddlValue' [allowFiltering]='true' (open)='onOpen($event)' (filtering)='onFiltering($event)'  [dataSource]='data' [fields]='fields' [placeholder]='text'>
   <template #footerTemplate="" let-data="">
   <div style="text-align:center;line-height:40px;background-color:#DCDCDC;">Search more result</div>
   </template>
-  </ej-dropdownlist>`
+  </ej-dropdownlist>
+  <h4>Type value</h4>
+  <input type="number" [value]='1000' (input)='onChange($event)' style="padding:5px;width:100%;margin-top: 20px;"/>
+  `
 })
 export class AppComponent {
+  @ViewChild('samples')
+
+  public ddlObj: DropDownListComponent;
 
   // defined the array of data
   public data: { [key: string]: Object }[];
+
+  public ddlValue: number = 1000;
+
+  public initialData: any;
+
+  public fullData: any = [];
+
   constructor() {
-    this.data = [];
     for (let i: number = 0; i < 10000; i++) {
-      this.data[i] = { Number: (1001 + i).toString(), Id: i };
+      this.fullData[i] = { Number: (1001 + i).toString(), Id: i };
     }
+
+    // Get the first 50 data items from fullData and assign it to this.initialData.
+    this.initialData = new DataManager(this.fullData).executeLocal(new Query().take(50));
+
+    // Get the DropDownList initial render value from fullData and assign it to this.initialData.
+    let secondData: any = new DataManager(this.fullData).executeLocal(new Query().where('Id', 'equal', this.ddlValue));
+
+    // Concat the this.initialData and secondData and then assign it to this.data for DropDownList dataSource
+    this.data = this.initialData.concat(secondData);
+
+    // Set the current DropDownList dataSource to this.initialData.
+    this.initialData = this.data;
   }
+
   // maps the appropriate column to fields property
   public fields: Object = { text: 'Number', value: 'Id' };
 
   //set the placeholder to DropDownList input
   public text: string = "Select a number";
 
-  public dataQuery: Query = new Query().take(50);
+  // Open event callback function
+  public onOpen: EmitType<PopupEventArgs> = (e: PopupEventArgs) => {
+    // override the popup position to avoid the confusion with bottom textbox
+    (this.ddlObj as any).popupObj.offsetY = -((this.ddlObj as any).inputWrapper.container.offsetHeight + 2); // 2 for border of textbox wrapper element
+  }
 
+  // filtering event callback function
   public onFiltering: EmitType<FilteringEventArgs> = (e: FilteringEventArgs) => {
     let query: Query = new Query();
     // frame the query based on search string with filter type.
@@ -38,5 +70,18 @@ export class AppComponent {
     e.updateData(this.data, query);
   }
 
+  public onChange(e: any) {
+    // Get the current value item from full data.
+    let secondData: any = new DataManager(this.fullData).executeLocal(new Query().where('Id', 'equal', e.target.valueAsNumber));
+
+    // Concat the current value item with initial loaded data.
+    this.data = this.initialData.concat(secondData);
+
+    // Set the current DropDownList dataSource to this.initialData.
+    this.initialData = this.data;
+
+    // Set the current numeric textbox value to DropDownList value property.
+    this.ddlValue = e.target.valueAsNumber;
+  }
 }
 
